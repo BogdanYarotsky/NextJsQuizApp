c48cf2a23683be0f98cfd490af759f424b6a3e01
BogdanYarotsky <byarotsky@gmail.com>
2023-10-26 10:09:49 +0200

    working preview of quiz page

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,91 @@
 "use client";
 import { useState } from "react";
+import { usePathname, useRouter } from 'next/navigation';
+
+function extractYouTubeId(url: string) {
+  try {
+    const parsedUrl = new URL(url);
+    let videoId = null;
+
+    if (parsedUrl.hostname === "www.youtube.com" || parsedUrl.hostname === "youtube.com") {
+      if (parsedUrl.pathname === "/watch") {
+        const searchParams = new URLSearchParams(parsedUrl.search);
+        videoId = searchParams.get('v');
+      } else if (parsedUrl.pathname.startsWith("/live/")) {
+        videoId = parsedUrl.pathname.split("/")[2];
+      }
+    }
+
+    return videoId;
+  } catch (error) {
+    console.error("Invalid URL");
+    return null;
+  }
+}
 
 export default function Home() {
-  const [inputFields, setInputFields] = useState<string[]>(['']);
+  const router = useRouter();
+  const [videoField, setVideoField] = useState<string>('');
+  const [roundFields, setRoundFields] = useState<string[]>(['']);
 
-  const handleInputChange = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
-    const values = [...inputFields];
+  const handleRoundChange = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
+    const values = [...roundFields];
     values[index] = event.target.value;
-    setInputFields(values);
-  };
-
-  const handleAddField = () => {
-    setInputFields([...inputFields, '']);
+    setRoundFields(values);
   };
 
   const handleRemoveField = () => {
-    if (inputFields.length < 2) return;
-    setInputFields(inputFields.slice(0, -1));
+    if (roundFields.length < 2) return;
+    setRoundFields(roundFields.slice(0, -1));
   };
+
+  const openLinkInNewTab = () => {
+    const video = extractYouTubeId(videoField);
+    const queryParams = {
+      video: video ?? "null",
+      rounds: "tbd"
+    }
+    const url = "/quiz?" + new URLSearchParams(queryParams).toString();
+    window.open(url, '_blank');
+  }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
       <div className="mb-2">
         <div>Лінк на трансляцію:</div>
         <input className="border rounded px-2 py-1 ml-2"
+          value={videoField}
+          onChange={event => setVideoField(event.target.value)}
           type="text"
         />
       </div>
       <div>
         <button
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full mr-2"
-          onClick={handleAddField}>Додати тур</button>
+          onClick={() => setRoundFields([...roundFields, ''])}>Додати тур</button>
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mr-2"
           onClick={handleRemoveField}>Прибрати тур</button>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2"
-          onClick={handleAddField}>Лінк на гру</button>
+          onClick={handleRemoveField}>Лінк</button>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2"
-          onClick={handleRemoveField}>Опис для телеграму</button>
+          onClick={handleRemoveField}>Опис</button>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleRemoveField}>Превью</button>
+          onClick={openLinkInNewTab}>Превью</button>
       </div>
-      {inputFields.map((field, index) => (
-        <div>
+      {roundFields.map((field, index) => (
+        <div key={index}>
           <div>Тур {index}:</div>
           <input className="border rounded px-2 py-1 ml-2"
-            key={index}
             type="text"
             value={field}
-            onChange={event => handleInputChange(index, event)}
+            onChange={event => handleRoundChange(index, event)}
           />
         </div>
       ))}
-      <div className="flex">
-
-      </div>
     </div>
   );
 }
--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
 
+const VideoPlayer = ({ video }: { video: string }) =>
+    <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />;
+
+
 export default function Quiz() {
     const searchParams = useSearchParams()
+    const video = searchParams.get('video');
 
     return (
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/DWFGa2ludLY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <>
+            {video && <VideoPlayer video={video} />}
+        </>
     )
 }
